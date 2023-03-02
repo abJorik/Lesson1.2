b2f111d031668b4be994729e4caa8fc45df2b911
Георгий <voproshalo@yandex.ru>
2023-03-02 19:15:24 +0300

    Name взято в кавычки

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("************************************");
         System.out.println("****\t     Визитка          \t****");
-        System.out.println("****\t     Name: Георгий    \t****");
+        System.out.println("****\t     Name: \"Георгий\"\t****");
         System.out.println("****\t     Skype: test     \t****");
         System.out.println("************************************");
     }
