5061d622434c22bdf8d253bdb7e3daa0114768f7
Георгий <voproshalo@yandex.ru>
2023-03-02 19:11:13 +0300

    Создана визитка

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("************************************");
+        System.out.println("****\t     Визитка          \t****");
+        System.out.println("****\t     Name: Георгий    \t****");
+        System.out.println("****\t     Skype: test     \t****");
+        System.out.println("************************************");
+    }
+}
